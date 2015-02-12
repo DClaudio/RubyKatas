e15d967d7f85eccf5dcf9dece764572c6450e8c5
unknown <claudio.david@LONWRK071.virtuefusion.corp>
2015-02-12 11:19:54 +0000

    further refactoring

--- lib/diamond_generator.rb
+++ lib/diamond_generator.rb
@@ -1,20 +1,19 @@
 class DiamondGenerator
-  STARTING_LETTER = 'A'
   def generate(letter)
     result = []
-    letter_index = letter.ord - STARTING_LETTER.ord
+    letter_distance = letter.ord - 'A'.ord
     if letter == 'A'
       result << 'A'
     else
-      (0..letter_index-1).each do |index|
-        output_letter = (STARTING_LETTER.ord + index).chr
+      (0..letter_distance-1).each do |index|
+        current_letter = ('A'.ord + index).chr
         if index == 0
-          result << ' '*letter_index + output_letter + ' '*letter_index
+          result << ' '*letter_distance + current_letter + ' '*letter_distance
         else
-          result << ' '*(letter_index-index) + output_letter + ' '*(2*index-1) + output_letter + ' '*(letter_index-index)
+          result << ' '*(letter_distance-index) + current_letter + ' '*(2*index-1) + current_letter + ' '*(letter_distance-index)
         end
       end
-      result = result + [letter + ' '*(2*letter_index-1) + letter] + result.reverse
+      result = result + [letter + ' '*(2*letter_distance-1) + letter] + result.reverse
     end
     result
   end
